cf08a86d1b2edf34a3c40bdec5d5ac1589b1a617
Grace Wang <t4_23@yahoo.com.tw>
2023-03-13 16:38:12 +0800

    deploy

---
+++ assets/OrderCheckout-9661cbb2.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-81e4f8a2.js";const e={};function c(t,_){return"1"}const o=r(e,[["render",c]]);export{o as default};
---
+++ assets/loadingStore-f8f0d968.js
@@ -0,0 +1,1 @@
+import{B as i}from"./index-81e4f8a2.js";const t=i("loadingStore",{state:()=>({isLoading:!1,loadingStatus:""}),actions:{loading(){this.isLoading=!0,setTimeout(()=>{this.isLoading=!1},400)}}});export{t as l};
