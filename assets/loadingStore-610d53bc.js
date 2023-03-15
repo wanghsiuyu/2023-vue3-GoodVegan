790efa2808eac3d9b6af051834dd121b652ddfb6
Grace Wang <t4_23@yahoo.com.tw>
2023-03-15 23:45:49 +0800

    deploy

---
+++ assets/loadingStore-610d53bc.js
@@ -0,0 +1,1 @@
+import{B as i}from"./index-72b02649.js";const t=i("loadingStore",{state:()=>({isLoading:!1,loadingStatus:""}),actions:{loading(){this.isLoading=!0,setTimeout(()=>{this.isLoading=!1},400)}}});export{t as l};
