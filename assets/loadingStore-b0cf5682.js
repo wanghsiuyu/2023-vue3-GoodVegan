cad3bfae8f75c210ab8a6089d5f3efd41426fa71
Grace Wang <t4_23@yahoo.com.tw>
2023-03-15 21:42:33 +0800

    deploy

---
+++ assets/loadingStore-b0cf5682.js
@@ -0,0 +1,1 @@
+import{B as i}from"./index-f15f06bb.js";const t=i("loadingStore",{state:()=>({isLoading:!1,loadingStatus:""}),actions:{loading(){this.isLoading=!0,setTimeout(()=>{this.isLoading=!1},400)}}});export{t as l};
--- assets/loadingStore-b2465be2.js
+++
@@ -1,1 +0,0 @@
-import{B as i}from"./index-9910186b.js";const t=i("loadingStore",{state:()=>({isLoading:!1,loadingStatus:""}),actions:{loading(){this.isLoading=!0,setTimeout(()=>{this.isLoading=!1},400)}}});export{t as l};
